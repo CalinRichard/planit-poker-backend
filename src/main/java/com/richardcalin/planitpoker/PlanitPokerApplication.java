2da837d9ea919608deb7a39275b63e6fe237780e
Richard <calin.richard@yahoo.com>
2023-06-16 11:02:34 +0300

    First commit

---
+++ src/main/java/com/richardcalin/planitpoker/PlanitPokerApplication.java
@@ -0,0 +1,13 @@
+package com.richardcalin.planitpoker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanitPokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanitPokerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/richardcalin/planitpoker/PlanitPokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.richardcalin.planitpoker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanitPokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
