72d8f6315af42cc4894dea3067f14650b43b38e3
Richard <calin.richard@yahoo.com>
2023-06-16 12:11:14 +0300

    Allow logging in with Google

--- src/main/java/com/richardcalin/planitpoker/PlanitPokerApplication.java
+++ src/main/java/com/richardcalin/planitpoker/PlanitPokerApplication.java
@@ -2,8 +2,10 @@ package com.richardcalin.planitpoker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
 @SpringBootApplication
+@EnableOAuth2Client
 public class PlanitPokerApplication {
 
 	public static void main(String[] args) {
